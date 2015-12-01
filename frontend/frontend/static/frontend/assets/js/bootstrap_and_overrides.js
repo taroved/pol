665cdc5d76eb9bc14eb2004a3cce32f394d8d5e4
Alexandr Nesterenko <kuchaspama@gmail.com>
2015-12-01 17:14:40 +0000

    picking in progress

--- frontend/frontend/static/frontend/assets/js/bootstrap_and_overrides.js
+++ frontend/frontend/static/frontend/assets/js/bootstrap_and_overrides.js
@@ -1,2 +1,4 @@
-$("a[rel~=popover], .has-popover").popover();
-$("a[rel~=tooltip], .has-tooltip").tooltip();
+$(document).ready(function(){
+    $("a[rel~=popover], .has-popover").popover();
+    $("a[rel~=tooltip], .has-tooltip").tooltip();
+});
