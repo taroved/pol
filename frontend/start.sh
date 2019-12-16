5067a3efbec6e43a606dd52911fccb31b4b3fdd1
Marllus Lustosa <marlluslustosa@riseup.net>
2019-12-16 19:42:35 +0000

    Update start.sh

--- frontend/start.sh
+++ frontend/start.sh
@@ -19,4 +19,4 @@ service nginx start > /dev/null
 /usr/bin/python manage.py loaddata fields.json 
 
 /usr/bin/python ../downloader.py &
-/usr/bin/python manage.py runserver 
+/usr/bin/python manage.py runserver
